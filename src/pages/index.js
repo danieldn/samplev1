06ffe3c5810d0d17e17f1ce457c610e46e1c94ab
Daniel Nguyen <danieldn@apple.com>
2021-11-22 09:16:45 -0800

    Finished Gatsby Tutorial Part 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,15 @@
 import * as React from "react";
 import Layout from "../components/layout";
-import { container } from "../components/layout.module.css";
+import { StaticImage } from "gatsby-plugin-image";
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
-      <p>I'm making this by following the Gatsby Tutorial.</p>
+      <p>iI'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage
+        src="https://i.natgeofe.com/n/3faa2b6a-f351-4995-8fff-36d145116882/domestic-dog_16x9.jpg"
+        alt="a puppy"
+      ></StaticImage>
     </Layout>
   );
 };
