547149b15c86764fa09f0234bade4006bf758207
Daniel Nguyen <danieldn@apple.com>
2021-11-24 08:34:16 -0800

    Finished Gatsby Tutorial Part 5

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,28 +1,43 @@
 import * as React from "react";
 import Layout from "../components/layout";
 import { graphql } from "gatsby";
+import { MDXRenderer } from "gatsby-plugin-mdx";
+
+// export const query = graphql`
+//   {
+//     allFile(filter: { sourceInstanceName: { eq: "blog" } }) {
+//       nodes {
+//         name
+//       }
+//     }
+//   }
+// `;
 
 export const query = graphql`
   {
-    allFile(filter: { sourceInstanceName: { eq: "blog" } }) {
+    allMdx(sort: { order: DESC, fields: frontmatter___date }) {
       nodes {
-        name
+        frontmatter {
+          date(formatString: "MMMM D, Y")
+          title
+        }
+        id
+        body
       }
     }
   }
 `;
 
 const BlogPage = ({ data }) => {
-  const blogTitles = data.allFile.nodes.map((node) => (
-    <li key={node.name}>{node.name}</li>
+  const blogs = data.allMdx.nodes.map((node) => (
+    <article key={node.id}>
+      <h1>{node.frontmatter.title}</h1>
+      <p>Posted: {node.frontmatter.date}</p>
+      <MDXRenderer>{node.body}</MDXRenderer>
+    </article>
   ));
 
-  return (
-    <Layout pageTitle="Blog">
-      My Blogs:
-      <ul>{blogTitles}</ul>
-    </Layout>
-  );
+  return <Layout pageTitle="My Blog Posts">{blogs}</Layout>;
 };
 
 export default BlogPage;
